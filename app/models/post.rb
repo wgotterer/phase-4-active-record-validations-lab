68b28d33be88cf091947e88bd4de7665acd160a9
William Gotterer <william.gotterer@gmail.com>
2021-11-23 11:03:42 -0500

    commit tree 00a4bed578439ba5a120c16a8b61f3ba100ef452

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,58 @@
+# class MyValidator < ActiveModel::Validator
+#     def validate(record)
+#         unless record.title.match?("You Won't Believe These True Facts")
+#             record.errors.add :name, "Has to be true facts!"
+#         end
+#     end
+# end
+
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :validate_title? 
+
+
+    # def validate_title
+    #     if title != "Won't Believe" 
+    #         errors.add :title, "Has to be true facts!"
+    #     end
+    # end
+
+    PATTERNS = [/Won't Believe/, /Secret/, /Top\d/, /Guess/]
+
+    def validate_title?
+        
+            if PATTERNS.none? {|pattern| pattern.match title}
+                  
+                errors.add :title, "Has to be true facts!"
+            end
+        
+    end
+
+
+    # def validate_title
+    #     if title 
+    #         unless title.match? (Won't Believe| Secret| Top\d| Guess)/
+    #             errors.add :title, "Has to be true facts!"
+    #         end
+    #     end
+        
+    # end
+    
+    
+end 
+
+
+
+
+
+
+
+ # def true_facts
+    #     unless title.match?("You Won't Believe These True Facts")
+
+    #     end
+    # end
